aaba4e54c0c258b8e372187a8738d87abda345a2
GeekCentric <ts22082@gmail.com>
2019-05-21 20:40:43 -0700

    set query selectors

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,16 @@
 var setFailedLogin = function() {
   document.getElementById('mainCard').innerHTML =
     '<h1 id="message" class="text-danger text-center">Intruder Detected</h1>'
-  document.getElementById('cardHeader').innerHTML = 'Intruder Detected'
-  document.getElementById('cardHeader').innerHTML = 'Notifying Authorities'
+  document.querySelector('#cardHeader').innerHTML = 'Intruder Detected'
+  document.querySelector('#cardHeader').innerHTML = 'Notifying Authorities'
 }
 
 //create a function for a successful login
 var setSuccessLogin = function() {
   document.getElementById('mainCard').innerHTML =
     '<h1 id="message" class="text-success text-center">User Authenticated</h1>'
-  document.getElementById('message').innerHTML = 'Access Granted'
-  document.getElementById('cardHeader').innerHTML = 'Success'
+  document.querySelector('#message').innerHTML = 'Access Granted'
+  document.querySelector('#cardHeader').innerHTML = 'Success'
 }
 
 // set login for handling a button click with id='loginBtn'
@@ -20,7 +20,7 @@ document.getElementById('loginBtn').addEventListener('click', function() {
   userName = document.getElementById('username').value
   password = document.getElementById('password').value
 
-  // both statements need to be true this is our very basic auth system
+  // both statements need to be true, this is our very basic auth system
   // we can totally hack this I can show you how!
   if (userName === 'admin' && password === 'adminadmin') {
     // if succesful do our success function
